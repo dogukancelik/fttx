cefd64fd995af6d76fd16c685078f41d6fe6be96
admin <admin@administratorpc>
2018-04-28 13:29:04 +0300

    dogukan

--- processManagement/WebContent/WEB-INF/classes/DBContext/dbContext.java
+++ processManagement/WebContent/WEB-INF/classes/DBContext/dbContext.java
@@ -1,11 +1,30 @@
 package DBContext;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+//import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class dbContext {
 	
-	public dbContext (String Conss) {
+	public dbContext () throws SQLException, ClassNotFoundException {
+		String Host = "jdbc:mysql://localhost:3306/"; 
+		String dbName = "menu";
+		String Username = "root";
+		String Password = "";
 		
+		Class.forName("com.mysql.jdbc.Connection");
+		Connection con =(Connection)DriverManager.getConnection(Host + dbName, Username, Password);	
 		
+		String query = "Select * from allmenu";
+		 PreparedStatement psmt = (PreparedStatement) con.prepareStatement(query);
+		 ResultSet rs = psmt.executeQuery();
 		
+		 while(rs.next()) {
+			 System.out.println(rs.getString(1));
+		 }
 	}
 	
 
