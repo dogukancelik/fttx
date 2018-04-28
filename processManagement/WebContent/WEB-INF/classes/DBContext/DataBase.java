3909eaf7b2b27459d7cfdf4412db9bb5914018ec
admin <admin@administratorpc>
2018-04-28 23:23:25 +0300

    dogukan

--- processManagement/WebContent/WEB-INF/classes/DBContext/DataBase.java
+++ processManagement/WebContent/WEB-INF/classes/DBContext/DataBase.java
@@ -9,30 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-/**
- * Servlet implementation class DataBase
- */
 @WebServlet("/DataBase")
 public class DataBase extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DataBase() {
         super();
        
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		try {
 			DBContext.dbContext.ConnectionOpen();
@@ -42,11 +29,8 @@ public class DataBase extends HttpServlet {
 		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
