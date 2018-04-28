324f7c7bf2077c8a52a47bf2b857df14b890a60d
admin <admin@administratorpc>
2018-04-28 20:09:15 +0300

    dogukan

---
+++ processManagement/WebContent/WEB-INF/classes/DBContext/DataBase.java
@@ -0,0 +1,53 @@
+package DBContext;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class DataBase
+ */
+@WebServlet("/DataBase")
+public class DataBase extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DataBase() {
+        super();
+       
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			DBContext.dbContext.ConnectionOpen();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- processManagement/WebContent/WEB-INF/classes/DBContext/dbContext.java
+++ processManagement/WebContent/WEB-INF/classes/DBContext/dbContext.java
@@ -1,31 +1,34 @@
 package DBContext;
 
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
+//import com.mysql.jdbc.PreparedStatement;
 //import com.mysql.jdbc.Statement;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class dbContext {
+public  class dbContext {
 	
-	public dbContext () throws SQLException, ClassNotFoundException {
-		String Host = "jdbc:mysql://localhost:3306/menu"; 
-		//String dbName = "menu";
+	private static java.sql.Connection con;
+	public static void ConnectionOpen() throws SQLException, ClassNotFoundException {
+		String Host = "jdbc:mysql://localhost:3306/"; 
+		String dbName = "menu";
 		String Username = "root";
 		String Password = "";
 		
-		Class.forName("com.mysql.jdbc.Connection");
-		Connection con =(Connection)DriverManager.getConnection(Host, Username, Password);	
+		Class.forName("com.mysql.jdbc.Driver");
+		 con =DriverManager.getConnection(Host + dbName, Username, Password);	
 		
-		String query = "Select * from allmenu";
+		/*String query = "Select * from allmenu";
 		 PreparedStatement psmt = (PreparedStatement) con.prepareStatement(query);
 		 ResultSet rs = psmt.executeQuery();
-		
 		 while(rs.next()) {
 			 System.out.println(rs.getString(1));
-		 }
+		 }*/
+		
+	}
+	public static void ConnectionClose() throws SQLException, ClassNotFoundException {
+		
+		con.close();
 	}
 	
 
