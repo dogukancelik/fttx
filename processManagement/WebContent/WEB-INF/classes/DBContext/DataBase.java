cb22937fe4617a84450a36b9b2a2542a526a2c5a
sahabay <sahabay@saha>
2018-04-29 09:49:30 +0300

    sami

--- processManagement/WebContent/WEB-INF/classes/DBContext/DataBase.java
+++ processManagement/WebContent/WEB-INF/classes/DBContext/DataBase.java
@@ -20,7 +20,7 @@ public class DataBase extends HttpServlet {
     }
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		try {
 			DBContext.dbContext.ConnectionOpen();
 		} catch (ClassNotFoundException | SQLException e) {
@@ -32,6 +32,7 @@ public class DataBase extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+		
 	}
 
 }
